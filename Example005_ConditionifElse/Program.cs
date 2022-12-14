a3619958d9c94705dc1b542ee3ce16b83860fc10
Shabdan <abdykulovshabdan@gmail.com>
2022-12-14 15:17:37 +0600

    add Example 005 Codition if-Else

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "шабдан")
+{
+    Console.WriteLine("Ура, это же Шабдан!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
